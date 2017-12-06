9952e90e50a39b631ed3d7120af0ba41702ddec3
Marcelo A Carrillo <marceloacarrillo@gmail.com>
2017-12-06 01:24:39 +0000

    new changes

--- hackernews/lib/hackernews/cli.rb
+++ hackernews/lib/hackernews/cli.rb
@@ -17,22 +17,22 @@ class HackerNews::CLI
       input = gets.strip.to_i #this is an input from the user that returns an integer value when the user select their option
       userarticle = HackerNews::NEWSLIST.find(input)
       articledetails(userarticle)
-
   end
+
+
   def listarticle
       HackerNews::NEWSLIST.all.each.with_index(1) do | articles,  index |
       puts "#{index}. #{articles.title}"
-
   end
-  end
+end
 
   def articledetails(userarticle)
     binding.pry
-     puts "url: #{userarticle.url}" if userarticle.url
-     puts "description: #{userarticle.description}" if userarticle.description
-     puts "source: #{userarticle.source}" if userarticle.source
-     puts "author: #{userarticle.author}" if userarticle.author
-     puts "title: #{userarticle.title}" if userarticle.title
+    puts "author: #{userarticle.author}" if userarticle.author
+    puts "title: #{userarticle.title}" if userarticle.title
+    puts "description: #{userarticle.description}" if userarticle.description
+    puts "source: #{userarticle.source}" if userarticle.source
+    puts "url: #{userarticle.url}" if userarticle.url
   end
 
 end
